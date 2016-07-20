24de1516c3cf66f6fea812af1ff0e3e47c6a7b65
fwjok <fwjok@163.com>
2016-07-20 22:54:09 +0800

    第一个加载图层的demo

--- GisMap/Form1.cs
+++ GisMap/Form1.cs
@@ -22,6 +22,7 @@ namespace GisMap
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Gis控件的动态加载
             AxMap axMap1 = new AxMap();
             ((System.ComponentModel.ISupportInitialize)(axMap1)).BeginInit();
             this.Controls.Add(axMap1);
@@ -32,11 +33,14 @@ namespace GisMap
             //   axMap1.Dock = System.Windows.Forms.DockStyle.Fill;
             axMap1.Size = new System.Drawing.Size(560, 413);
             //     axMap1.TabIndex = 2;
-
+            
+            //GisSet控件的动态加载
             GisSetControl m_GisSetControl = new GisSetControl();
             this.Controls.Add(m_GisSetControl);
             m_GisSetControl.Location = new System.Drawing.Point(0, 400);
             m_GisSetControl.Size = new System.Drawing.Size(560, 100);
+
+            m_GisSetControl.MAP = axMap1;
           
         }
     }
